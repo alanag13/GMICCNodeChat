017831c22c21b02ac435de37debb388b6d3b9b6c
Alan Grgic <alan.grgic@gmail.com>
2015-02-05 00:18:38 -0600

    Initial UI and basic chat functionality

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -1,6 +1,18 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function (socket) {
+    socket.on('message-sent', function (msg) {
+        io.emit('message-received', msg);
+    });
+});
+
+http.listen(1337, function () {
+    console.log('listening on *:1337');
+});
