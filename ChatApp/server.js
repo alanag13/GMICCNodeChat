fa8db7392fca056977d4bd6acb733ce9366432e2
Alan Grgic <alan.grgic@gmail.com>
2015-02-05 01:26:28 -0600

    Add mega helpful comments

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -6,7 +6,9 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-
+//every time someone connects, set it up so that
+//every time they send a message, we tell all connected clients
+//that we recieved it (and what it was)
 io.on('connection', function (socket) {
     socket.on('message-sent', function (msg) {
         io.emit('message-received', msg);
