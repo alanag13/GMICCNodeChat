3b928cb7ade3e74f42d81527f235fec18b85f57a
Alan Grgic <alan.grgic@gmail.com>
2015-02-05 07:51:22 -0600

    Add screenname support

--- ChatApp/ChatViewModel.js
+++ ChatApp/ChatViewModel.js
@@ -2,10 +2,53 @@
 ChatApp.ViewModel = function () {
     
     var messages = ko.observableArray([]);
+    var userCount = ko.observable(0);
     var inputboxText = ko.observable();
+    var currentUserName = ko.observable();
+    
+    var submitUserName = function (){
+
+        var user = $('#screenname-input').val();
+        user = user.replace(/ /g, ''); //remove whitespace characters
+
+        if (user && user.trim() != '') {
+            socket.emit('submit-username', user);
+        }
+    }
+
+    var onUserNameCreate = function (user) {
+        
+        //if the user name wasn't already taken let the user start chatting
+        vm.currentUserName(user);
+        vm.userCount(vm.userCount() + 1);    
+        $('#screenNameDialog').modal('hide');
+        socket.emit('message-sent', { sender: "", message : '********** ' + user + ' joined the chat. ********** ' });
+    }
+    
+    var sendMessage = function (){
+        if (!inputboxText() || inputboxText().trim() == "") return false;
+        socket.emit('message-sent', { sender: currentUserName(), message : inputboxText() });
+        inputboxText('');
+        return false;
+    }
+    
+    var onUserNameReject = function (user) {
+        //try a different name
+    }
+        
+    var onUserDisconnect = function (user){
+        userCount(userCount()--);
+        //x left the chat
+    }
     
     return {
         messages : messages,
-        inputboxText : inputboxText
+        userCount : userCount,
+        inputboxText : inputboxText,
+        currentUserName : currentUserName,
+        submitUserName : submitUserName,
+        onUserNameCreate : onUserNameCreate,
+        sendMessage : sendMessage,
+        onUserDisconnect : onUserDisconnect
     }
 };
--- ChatApp/server.js
+++ ChatApp/server.js
@@ -2,24 +2,46 @@
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = [];
+var displayNames = [];
+
 //send index if at the root of the domain
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 //for everything else send whatever matches the url of what was requested
-app.get('/*', function (req, res){
+app.get('/*', function (req, res) {
     res.sendFile(__dirname + req.url)
-})
+});
 
-//every time someone connects, set it up so that
-//every time they send a message, we tell all connected clients
-//that we recieved it (and what it was)
+
 io.on('connection', function (socket) {
+    //every time someone connects, set it up so that
+    //every time they send a message, we tell all connected clients
+    //that we recieved it (and what it was)
     socket.on('message-sent', function (msg) {
         io.emit('message-received', msg);
     });
+    
+    //check to see if a username is already taken.
+    socket.on('submit-username', function (user) {
+        if (!users[user.toUpperCase()]) {
+            users.push(user.toUpperCase());
+            displayNames.push(user.sender);
+            
+            io.emit('screen-name-approved', user)
+
+        //if it is, tell the user to pick a different one
+        } else {
+        user.socket.emit('screen-name-rejected', user)
+        }
+    });
 });
+
+
+
+
 
 http.listen(1337, function () {
     console.log('listening on *:1337');
