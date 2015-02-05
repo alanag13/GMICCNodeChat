afaeb27857d310459ce314110ed13a35a9508884
Alan Grgic <alan.grgic@gmail.com>
2015-02-05 02:02:43 -0600

    add cookies module for client

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -2,9 +2,15 @@
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//send index if at the root of the domain
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+//for everything else send whatever matches the url of what was requested
+app.get('/*', function (req, res){
+    res.sendFile(__dirname + req.url)
+})
 
 //every time someone connects, set it up so that
 //every time they send a message, we tell all connected clients
